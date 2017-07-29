678d15760fbb42cd3124b7a1f27cea6f5e33bb3f
Konrad <konradam@o2.pl>
2017-07-29 18:23:15 +0200

    Dodanie projektów Domain i Domain.EntityFramework

---
+++ MusicService/Models/Album.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicService.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Band { get; set; }
+        public string Genre { get; set; }
+        public DateTime PublicationDate { get; set; }
+    }
+}
