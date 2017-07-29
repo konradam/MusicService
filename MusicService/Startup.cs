e4b47bf8d5dc32b9841535df1351ed819a70faae
Konrad <konradam@o2.pl>
2017-07-29 17:45:46 +0200

    Add project files.

---
+++ MusicService/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MusicService.Startup))]
+namespace MusicService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
